955843197580fe3cf4d2ac1e1cf25fcd30087a8f
AndreasSchoenfeldUser <andreas.schoenfeld@comquent.de>
2018-01-19 23:39:54 +0100

    Update locale.groovy

--- locale.groovy
+++ locale.groovy
@@ -1,1 +1,10 @@
 
+import org.jvnet.localizer.LocaleProvider;
+import java.util.Locale;
+
+LocaleProvider.setProvider(new LocaleProvider() {
+    LocaleProvider original = LocaleProvider.getProvider();
+    public Locale get() {
+		     return new Locale("EN")
+    }
+});
