12489fb66fc407cdbfe198aa7b9a2652fec43a64
robert <robert@robert-PC.mshome.net>
2018-02-02 17:30:21 +0100

    fix error

--- docker.groovy
+++ docker.groovy
@@ -1,4 +1,5 @@
 import org.jenkinsci.plugins.docker.commons.tools.DockerTool
 
 def dockerInstallation = new DockerTool("docker", "/usr/bin", null)
+def dockerDescriptor = Jenkins.instance.getDescriptorByName("org.jenkinsci.plugins.docker.commons.tools.DockerTool")
 dockerDescriptor.setInstallations(dockerInstallation)
