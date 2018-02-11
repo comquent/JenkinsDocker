42383e716efd61327eb449ca48c8405fcae1974a
robert <robert@robert-PC.mshome.net>
2018-02-11 16:02:26 +0100

    add startup trigger

--- job.groovy
+++ job.groovy
@@ -4,14 +4,20 @@ import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
 import hudson.plugins.git.GitSCM
 import org.jenkinsci.plugins.pipeline.modeldefinition.config.FolderConfig
 import javaposse.jobdsl.plugin.ExecuteDslScripts
+import org.jvnet.hudson.plugins.triggers.startup.HudsonStartupTrigger
 
 def job = Jenkins.get().createProject(FreeStyleProject, 'seed-job')
-def builder = new ExecuteDslScripts([targets: 'jobs.seed'])
+
 def scm = new GitSCM('${SCM_URL}')
 job.scm = scm
+
+def builder = new ExecuteDslScripts([targets: 'jobs.seed'])
 job.buildersList.add(builder)
+
+def trigger = new HudsonStartupTrigger('master', '0', '', '')
+job.addTrigger(trigger)
 
 job.save()
 Jenkins.get().reload()
 
-Jenkins.get().queue.schedule(job)
+//Jenkins.get().queue.schedule(job)
