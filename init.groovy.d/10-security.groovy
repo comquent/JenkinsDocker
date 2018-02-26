a6d8d3f0f922b9876af2debdbbf3de796c23c947
robertschimpfle <robert.schimpfle@comquent.de>
2018-02-26 16:41:37 +0100

    Update 10-security.groovy

--- init.groovy.d/10-security.groovy
+++ init.groovy.d/10-security.groovy
@@ -12,3 +12,18 @@ user.save()
  
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
 jenkins.save()
+
+def resetEnvironmentVariable(String name, String value) {
+        def processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+        def theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+        theEnvironmentField.setAccessible(true);
+        def env = (Map<String, String>) theEnvironmentField.get(null);
+        env.put(name, value);
+        def theCaseInsensitiveEnvironmentField = processEnvironmentClass
+                .getDeclaredField("theCaseInsensitiveEnvironment");
+        theCaseInsensitiveEnvironmentField.setAccessible(true);
+        def cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
+        cienv.put(name, value);
+    }
+
+resetEnvironmentVariable('JENKINS_PASS', '-----')
